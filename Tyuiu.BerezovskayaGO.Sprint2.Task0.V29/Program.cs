6b8d77c9b1b3961ee9937351b4df2a95bdaa6565
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-10-01 19:08:11 +0500

    Add project files.

---
+++ Tyuiu.BerezovskayaGO.Sprint2.Task0.V29/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.BerezovskayaGO.Sprint2.Task0.V29.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint2.Task0.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Березовская Г. О. | АСОиУБ - 24-1";
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                                 *");
+            Console.WriteLine("* Задание #0                                                                               *");
+            Console.WriteLine("* Вариант #29                                                                              *");
+            Console.WriteLine("* Выполнила: Березовская Галина Олеговна | АСОиУБ - 24-1                                   *");
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                 *");
+            Console.WriteLine("* Написать программу из операций сравнения и арифметических выражений,                     *");
+            Console.WriteLine("* которая вернет логическую последовательность(массива).                                   *");
+            Console.WriteLine("*                                                                                          *");
+
+            int x = 105;
+            int y = 735;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                         *");
+            Console.WriteLine("********************************************************************************************");
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+
+            Console.WriteLine("********************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+            Console.WriteLine("********************************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
