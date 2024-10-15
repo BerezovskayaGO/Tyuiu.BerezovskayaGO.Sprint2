3fa4d70e2d99f6a4d2daae9dcd3eb7eee113fa24
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-10-15 16:21:40 +0500

    Task3

---
+++ Tyuiu.BerezovskayaGO.Sprint2.Task3.V26.Test/DataServiceTest.cs
@@ -0,0 +1,47 @@
+using Tyuiu.BerezovskayaGO.Sprint2.Task3.V26.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint2.Task3.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 4.702;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 1.667;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = -4.754;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -31;
+            double res = ds.Calculate(x);
+            double wait = -247.968;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
